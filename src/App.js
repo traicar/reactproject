42e6dad4c3572805efba2d490e12c8dc79eab2b6
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-06-03 12:55:53 -0500

    completed to do list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react'
-import header from './components/HeaderComponent'
-import footer from './components/FooterComponent'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  }
   return (
-    <div className="container">
-      <h1>Productivity Sheet</h1>
+    <div className="App">
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
     </div>
   );
-  }
+}
 
 export default App;
--- src/components/MainComponent.js
+++
@@ -1,6 +0,0 @@
-import React {component}, from 'react';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import DailySheet from './DailySheetComponent';
-import Productivity from './ProductivitySheetComponent';
-import List from './ToDoListAboutComponent';
